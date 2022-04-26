3a90d5c4321f7d6901f41ca273be81dc7827c34c
yogesh.bhattrai <yogesh.bhattrai@aitcentre.com>
2022-04-26 20:58:12 +0545

    updating the routes to userroutes

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,15 @@ const express = require("express");
 const AddOrder = require("./order/addOrder/AddOrder");
 const registerUser = require("./register/register.route.js");
 const Response = require("./Response.js");
-const router = express.Router();
+const UserRouter = express.Router();
 const validateUser = require("./validateUser/validateUser");
 
-const UserRoutes = () => {
-  router.post("/login", (req, res) => login(req, res, Response));
-  router.post("/createuser", (req, res) => registerUser(req, res, Response));
-  router.post("/validateuser", (req, res) => validateUser(req, res, Response));
-  router.post("/add__order", (req, res) => AddOrder(req, res, Response));
-  router.get("/", (req, res) => res.send("Server has started"));
-};
+UserRouter.post("/login", (req, res) => login(req, res, Response));
+UserRouter.post("/createuser", (req, res) => registerUser(req, res, Response));
+UserRouter.post("/validateuser", (req, res) =>
+  validateUser(req, res, Response)
+);
+UserRouter.post("/add__order", (req, res) => AddOrder(req, res, Response));
+UserRouter.get("/", (req, res) => res.send("Server has started"));
 
-module.exports = UserRoutes;
+module.exports = UserRouter;
